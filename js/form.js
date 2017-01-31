7601ad02aea3d5dfec7a147a722611802b2cc5c9
Alexandr <stwork87@gmail.com>
2017-01-31 12:55:50 +0300

    after mistakes corrections

--- js/form.js
+++ js/form.js
@@ -17,14 +17,6 @@ var uploadForm = uploadOverlay.querySelector('form');
 // getting array of the filter input
 var controls = uploadForm.elements['upload-filter'];
 
-// filters list
-var filters =
-  ['filter-chrome',
-    'filter-sepia',
-    'filter-marvin',
-    'filter-phobos',
-    'filter-heat'];
-
 // zoom button
 var zoomButton = document.querySelector('.upload-resize-controls-button-inc');
 
@@ -38,17 +30,17 @@ var imageSizeField = document.querySelector('.upload-resize-controls-value');
 var imageSizeValue = 100;
 var imageSizeStep = 25;
 var minImageSize = 25;
-var maxImageSize = 25;
+var maxImageSize = 100;
 
 uploadOverlay.classList.add('invisible');
 uploadSelectImage.classList.remove('invisible');
 
 // changing the value of the upload file field
-if (uploadFile !== 0) {
+uploadFile.addEventListener('click', function () {
+  event.preventDefault();
   uploadOverlay.classList.remove('invisible');
   uploadSelectImage.classList.add('invisible');
-}
-
+});
 // closing image button event
 uploadFormCancel.addEventListener('click', function () {
   uploadOverlay.classList.add('invisible');
@@ -69,31 +61,22 @@ function checkControl(control) {
 
 function toogleFilter(control) {
   if (control === 0) {
-    console.log('первый');
     return;
   }
-  mainImage.className = '';
-  mainImage.classList.add('filter-image-preview');
+  var deletedClass = mainImage.classList.item(1);
+  mainImage.classList.remove(deletedClass);
   addingClass(control);
 }
 
 // adding class of the new filter to main image
 function addingClass(control) {
-  // seaching number of the member of the array
-  for (var z = 0; z < controls.length; z++) {
-    if (controls[z] === control) {
-      mainImage.classList.add(filters[z - 1]);
-      break;
-    }
-  }
+  mainImage.classList.add('filter-' + control.value);
 }
 
 // decreasing image
 decreaseButton.addEventListener('click', function () {
   if (imageSizeValue > minImageSize) {
     imageSizeValue -= imageSizeStep;
-    console.log('---'); // for debugging
-    console.log(imageSizeValue);
     resizeMainPicture();
   }
 });
@@ -102,8 +85,6 @@ decreaseButton.addEventListener('click', function () {
 zoomButton.addEventListener('click', function () {
   if (imageSizeValue < maxImageSize) {
     imageSizeValue += imageSizeStep;
-    console.log('+++'); // for debugging
-    console.log(imageSizeValue);
     resizeMainPicture();
   }
 });
