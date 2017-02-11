40636d44d65c0bba5dd69ee4de41e0d30594899f
Левшенко Александр < >
2017-02-10 22:30:45 -0800

    renaming functions

--- js/form.js
+++ js/form.js
@@ -2,21 +2,21 @@
 var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadSelectImage = document.getElementById('upload-select-image');
 var uploadFile = document.querySelector('.upload-file');
-// closing image button
-var buttonUploadFormClose = document.querySelector('.upload-form-cancel');
 
-var mainImage = document.querySelector('.upload-form-preview > img');
+var buttonCloseModal = document.querySelector('.upload-form-cancel');
+
+var image = document.querySelector('.upload-form-preview > img');
 
 var uploadForm = uploadOverlay.querySelector('form');
 
-var clickableRadioArea = document.querySelectorAll('.upload-filter-preview');
+var filters = document.querySelectorAll('.upload-filter-preview');
 
-var changeFilterButtonGroup = uploadForm.elements['upload-filter'];
+var filterButtons = uploadForm.elements['upload-filter'];
 
-var controlsWrapper = document.querySelector('.upload-filter-controls');
-var zoomButton = document.querySelector('.upload-resize-controls-button-inc');
-var decreaseButton = document.querySelector('.upload-resize-controls-button-dec');
-var imageSizeField = document.querySelector('.upload-resize-controls-value');
+var filterLabels = document.querySelector('.upload-filter-controls');
+var increasingScaleButton = document.querySelector('.upload-resize-controls-button-inc');
+var decreasingScaleButton = document.querySelector('.upload-resize-controls-button-dec');
+var imageSize = document.querySelector('.upload-resize-controls-value');
 
 // default values
 var imageSizeValue = 100;
@@ -29,78 +29,77 @@ var ESC_KEY_CODE = 27;
 
 uploadOverlay.classList.add('invisible');
 uploadSelectImage.classList.remove('invisible');
-uploadFile.addEventListener('click', onClickImageOpening);
-uploadFile.addEventListener('keydown', onEnterImageOpening);
-buttonUploadFormClose.addEventListener('click', onClickUploadImageClosing);
-document.addEventListener('keydown', onEscapeImageClosing);
-controlsWrapper.addEventListener('click', onClickChangeImageFilter);
-controlsWrapper.addEventListener('keydown', onEnterChangeImageFilter);
-decreaseButton.addEventListener('click', decreaseImage);
-zoomButton.addEventListener('click', increaseImage);
+uploadFile.addEventListener('click', onImageOpen);
+uploadFile.addEventListener('keydown', onImageOpenByEnter);
+buttonCloseModal.addEventListener('click', onClose);
+document.addEventListener('keydown', onCloseByEscape);
+filterLabels.addEventListener('click', onSelectFilter);
+filterLabels.addEventListener('keydown', onSelectFilterByEnter);
+decreasingScaleButton.addEventListener('click', decreaseScale);
+increasingScaleButton.addEventListener('click', increaseScale);
 
 function toggleFilter(target) {
   deleteFilter();
   // make all attributes area-checked false
-  deletingAreaChecked(clickableRadioArea);
-  togglegAriaChecked(target);
-  mainImage.classList.add('filter-' + target.parentNode.previousElementSibling.value);
+  deleteAreaChecked(filters);
+  toggleAriaChecked(target);
+  image.classList.add('filter-' + target.parentNode.previousElementSibling.value);
   changeInputChecked(target);
 }
 
 function deleteFilter() {
-  for (var i = 0; i < changeFilterButtonGroup.length; i++) {
-    mainImage.classList.remove('filter-' + changeFilterButtonGroup[i].value);
+  for (var i = 0; i < filterButtons.length; i++) {
+    image.classList.remove('filter-' + filterButtons[i].value);
   }
 }
 
-function deletingAreaChecked(array) {
+function deleteAreaChecked(array) {
   for (var i = 0; i < array.length; i++) {
     array[i].setAttribute('aria-checked', 'false');
   }
 }
 
-// toggling aria-checked
-function togglegAriaChecked(element) {
+function toggleAriaChecked(element) {
   var pressed = (element.getAttribute('aria-checked') === 'true');
   element.setAttribute('aria-checked', !pressed);
 }
 
 function changeInputChecked() {
-  for (var i = 0; i < clickableRadioArea.length; i++) {
-    var targetRadioInput = clickableRadioArea[i].parentNode.previousElementSibling;
+  for (var i = 0; i < filters.length; i++) {
+    var targetRadioInput = filters[i].parentNode.previousElementSibling;
     if (targetRadioInput.hasAttribute('checked')) {
       targetRadioInput.removeAttribute('checked');
     }
   }
 }
 
-function increaseImage() {
+function increaseScale() {
   if (imageSizeValue < maxImageSize) {
     imageSizeValue += imageSizeStep;
-    resizeMainPicture(imageSizeValue / 100);
+    resizeImage(imageSizeValue / 100);
   }
 }
 
-function decreaseImage() {
+function decreaseScale() {
   if (imageSizeValue > minImageSize) {
     imageSizeValue -= imageSizeStep;
-    resizeMainPicture(imageSizeValue / 100);
+    resizeImage(imageSizeValue / 100);
   }
 }
 
-function resizeMainPicture(size) {
-  mainImage.style.transform = 'scale(' + size + ')';
-  mainImage.style.webkitTransform = 'scale(' + size + ')';
-  imageSizeField.value = size * 100 + '%';
+function resizeImage(size) {
+  image.style.transform = 'scale(' + size + ')';
+  image.style.webkitTransform = 'scale(' + size + ')';
+  imageSize.value = size * 100 + '%';
 }
 
-function onClickImageOpening() {
+function onImageOpen() {
   event.preventDefault();
   uploadImageOpening();
 }
 
-function onEnterImageOpening(event) {
-  if (isEnterEvent(event)) {
+function onImageOpenByEnter(event) {
+  if (isEnterKey(event)) {
     event.preventDefault();
     uploadImageOpening();
   }
@@ -111,34 +110,34 @@ function uploadImageOpening() {
   uploadSelectImage.classList.add('invisible');
 }
 
-function uploadImageClosing() {
+function closeUploadImage() {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
 }
 
-function onClickUploadImageClosing() {
-  uploadImageClosing();
+function onClose() {
+  closeUploadImage();
   // deleting unnessesary styles
   deleteFilter();
-  resizeMainPicture(1);
+  resizeImage(1);
 }
 
-function onEscapeImageClosing(event) {
-  if (isEscapeEvent(event)) {
-    uploadImageClosing();
+function onCloseByEscape(event) {
+  if (isEscapeKey(event)) {
+    closeUploadImage();
     // deleting unnessesary styles
     deleteFilter();
-    resizeMainPicture(1);
+    resizeImage(1);
   }
 }
 
-function onEnterChangeImageFilter(event) {
-  if (isEnterEvent(event)) {
+function onSelectFilterByEnter(event) {
+  if (isEnterKey(event)) {
     toggleFilter(event.target);
   }
 }
 
-function onClickChangeImageFilter(event) {
+function onSelectFilter(event) {
   event.preventDefault();
   if (!event.target.classList.contains('upload-filter-preview')) {
     return;
@@ -146,10 +145,10 @@ function onClickChangeImageFilter(event) {
   toggleFilter(event.target);
 }
 
-function isEnterEvent(evt) {
+function isEnterKey(evt) {
   return (evt.keyCode && evt.keyCode === ENTER_KEY_CODE);
 }
 
-function isEscapeEvent(evt) {
+function isEscapeKey(evt) {
   return (evt.keyCode && evt.keyCode === ESC_KEY_CODE);
 }
