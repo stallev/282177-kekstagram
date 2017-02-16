990f6526b3b61bdefb922d62de3d64a47e305ef0
stallev <stwork87@gmail.com>
2017-02-16 00:22:17 -0800

    IIFE realization partly  without mistakes

--- js/form.js
+++ js/form.js
@@ -1,6 +1,8 @@
 'use strict';
 var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadSelectImage = document.getElementById('upload-select-image');
+var image = document.querySelector('.upload-form-preview > img');
+var imageSize = document.querySelector('.upload-resize-controls-value');
 var uploadFile = document.querySelector('.upload-file');
 var buttonCloseModal = document.querySelector('.upload-form-cancel');
 var scaleControls = document.querySelector('.upload-resize-controls');
@@ -20,7 +22,7 @@ uploadFile.addEventListener('keydown', onOpenByEnter);
 buttonCloseModal.addEventListener('click', onClose);
 document.addEventListener('keydown', onCloseByEscape);
 
-(window.initializeScale(scaleControls, imageSizeStep, imageSizeValue))();
+window.initializeScale(scaleControls, imageSizeStep, imageSizeValue);
 
 function onOpen() {
   event.preventDefault();
@@ -50,7 +52,7 @@ function onClose() {
   close();
   // deleting unnessesary styles
   window.initializeFilters.deleteFilter();
-  window.initializeScale.resizeImage(1);
+  resizeImage(1);
 }
 
 function onCloseByEscape(event) {
@@ -58,8 +60,14 @@ function onCloseByEscape(event) {
     close();
     // deleting unnessesary styles
     window.initializeFilters.deleteFilter();
-    window.initializeScale.resizeImage(1);
+    resizeImage(1);
   }
+}
+
+function resizeImage(size) {
+  image.style.transform = 'scale(' + size + ')';
+  image.style.webkitTransform = 'scale(' + size + ')';
+  imageSize.value = size * 100 + '%';
 }
 
 function isEscapeKey(evt) {
