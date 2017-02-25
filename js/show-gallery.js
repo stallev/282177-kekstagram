d81ee8a4efc10029ca65d47715b66e450a3e77ce
stallev <stwork87@gmail.com>
2017-02-25 12:18:54 +0300

    delete object gallery

--- js/show-gallery.js
+++ js/show-gallery.js
@@ -3,40 +3,42 @@ window.showGallery = (function () {
   var ENTER_KEY_CODE = 13;
   var ESC_KEY_CODE = 27;
 
-  var gallery = {
-    overlay: document.querySelector('.gallery-overlay'),
-    closeButton: document.querySelector('.gallery-overlay-close'),
-    preview: document.querySelector('.gallery-overlay-preview'),
-    previewImage: document.querySelector('.gallery-overlay-image'),
-    previewLikesCount: document.querySelector('.likes-count'),
-    previewCommentsCount: document.querySelector('.comments-count'),
-    close: function () {
-      gallery.overlay.classList.add('invisible');
-    },
-    onClose: function () {
-      gallery.close();
-    },
-    onKeyClose: function (event) {
-      if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
-        gallery.close();
-      }
-    },
-    onCloseByEscape: function (event) {
-      if (event.keyCode && event.keyCode === ESC_KEY_CODE) {
-        gallery.close();
-      }
+  var overlay = document.querySelector('.gallery-overlay');
+  var closeButton = document.querySelector('.gallery-overlay-close');
+  var previewImage = document.querySelector('.gallery-overlay-image');
+  var previewLikesCount = document.querySelector('.likes-count');
+  var previewCommentsCount = document.querySelector('.comments-count');
+
+  function close() {
+    overlay.classList.add('invisible');
+  }
+
+  function onClose() {
+    close();
+  }
+
+  function onKeyClose() {
+    if (event.keyCode && event.keyCode === ENTER_KEY_CODE) {
+      close();
     }
-  };
-  gallery.closeButton.addEventListener('click', gallery.onClose);
-  gallery.closeButton.addEventListener('keydown', gallery.onKeyClose);
-  document.addEventListener('keydown', gallery.onCloseByEscape);
+  }
+
+  function onCloseByEscape() {
+    if (event.keyCode && event.keyCode === ESC_KEY_CODE) {
+      close();
+    }
+  }
+
+  closeButton.addEventListener('click', onClose);
+  closeButton.addEventListener('keydown', onKeyClose);
+  document.addEventListener('keydown', onCloseByEscape);
 
   return function (element) {
-    gallery.overlay.classList.remove('invisible');
-    gallery.closeButton.focus();
+    overlay.classList.remove('invisible');
+    closeButton.focus();
 
-    gallery.previewImage.src = element.url;
-    gallery.previewCommentsCount.innerHTML = element.comments.length;
-    gallery.previewLikesCount.innerHTML = element.likes;
+    previewImage.src = element.url;
+    previewCommentsCount.innerHTML = element.comments.length;
+    previewLikesCount.innerHTML = element.likes;
   };
 })();
